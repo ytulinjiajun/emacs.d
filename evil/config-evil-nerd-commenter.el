8051ab98bf3b5d1e234ea8f0eb5efa145c1c5345
ytulinjiajun <ytulinjiajun@163.com>
2017-09-22 19:35:59 +0800

    -----

---
+++ evil/config-evil-nerd-commenter.el
@@ -0,0 +1,4 @@
+(evilnc-default-hotkeys)
+(define-key evil-normal-state-map (kbd "SPC /") 'evilnc-comment-or-uncomment-lines)
+(define-key evil-visual-state-map (kbd "SPC /") 'evilnc-comment-or-uncomment-lines)
+(provide 'config-evil-nerd-commenter)
