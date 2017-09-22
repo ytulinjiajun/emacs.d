ae1c32a2b284c241aed415ffb5b9bef4cf9f565f
ytulinjiajun <ytulinjiajun@163.com>
2017-09-22 20:17:49 +0800

    -----

--- evil/config-evil-mode.el
+++ evil/config-evil-mode.el
@@ -7,8 +7,22 @@
 (setcdr evil-insert-state-map nil)
 (define-key evil-insert-state-map [escape] 'evil-normal-state)
 
+;;------------------------------------------------------------------
+;; 该配置改善occur-mode下面无法使用evil的问题 
+;;------------------------------------------------------------------
+(add-hook 'occur-mode-hook
+		  (lambda ()
+			(evil-add-hjkl-bindings occur-mode-map 'emacs
+			  (kbd "/") 'evil-search-forward
+			  (kbd "n") 'evil-search-next
+			  (kbd "N") 'evil-search-previous
+			  (kbd "C-d") 'evil-scroll-down
+			  (kbd "C-u") 'evil-scroll-up)))  
+
 (require 'config-evil-leader-plugin)
 
 (require 'config-evil-surround-plugin)
+
+(require 'config-evil-nerd-commenter)
 
 (provide 'config-evil-mode)
